693c3f0662613286abfb22f73f84d09f0040100b
Rafael Ramos <junior07.rrn@hotmail.com>
2024-04-11 12:42:52 -0400

    Add LogIn page

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import "./NavBar.scss";
+
+export default function NavBar() {
+    return (
+        <nav className="nav">
+            <ul className="nav-list"></ul>
+        </nav>
+    )
+}
--- src/pages/LogInPage/LogInPage.jsx
+++ src/pages/LogInPage/LogInPage.jsx
@@ -1,5 +1,32 @@
+import NavBar from "../../components/NavBar/NavBar";
 import "./LogInPage.scss";
+import espyLogo from "../../assets/images/espy-logo.png";
 
 export default function LogInPage() {
-  return <h1>Log In Page</h1>;
+  return (
+    <>
+      <header className="header">
+        <nav className="nav"></nav>
+      </header>
+      <main className="main">
+        <img src={espyLogo} alt="company-logo" className="main__logo" />
+        <form className="logIn-card">
+            <h1 className="logIn-card__header">Log In</h1>
+          <section className="logIn-card__container">
+            <label className="logIn-card__label" htmlFor="email">
+              Email:
+              <input className="logIn-card__input" type="text" name="email" />
+            </label>
+          </section>
+          <section className="logIn-card__container">
+            <label htmlFor="password" className="logIn-card__label">
+              Password:
+            </label>
+            <input type="text" className="logIn-card__input" name="password" />
+          </section>
+          <button className="logIn-card__btn">Log In</button>
+        </form>
+      </main>
+    </>
+  );
 }
